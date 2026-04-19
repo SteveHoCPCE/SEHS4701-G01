ac272835ca19f7fdaa5a88f57840dc33cc0d3e57
kym1234567890 <kymyy17@gmail.com>
2026-04-19 23:21:23 +0800

    update import on SecurityConfig.java

--- backend/src/main/java/com/sehs4701/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/sehs4701/backend/config/SecurityConfig.java
@@ -22,6 +22,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
